b0fa365ded1aa74de9666c644bd3d937ebdb8dd4
ayka2858 <105168256+ayka2858@users.noreply.github.com>
2022-08-07 14:42:20 +0300

    made some corrections

--- src/day0/HelloWorld.java
+++ src/day0/HelloWorld.java
@@ -2,6 +2,6 @@ package day0;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello World");
+        System.out.println("hello World. How are you today?");
     }
 }
--- src/day0/JamesBond.java
+++ src/day0/JamesBond.java
@@ -3,5 +3,6 @@ package day0;
 public class JamesBond {
     public static void main(String[] args) {
         System.out.println("Java is cool language");
+        System.out.println("Phyton is not");
     }
 }
