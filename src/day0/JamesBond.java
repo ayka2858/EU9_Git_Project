a3a67262e75628137a2cae575d34075776426616
ayka2858 <105168256+ayka2858@users.noreply.github.com>
2022-08-07 14:58:39 +0300

    made some corrections to JamesBond.java

--- src/day0/JamesBond.java
+++ src/day0/JamesBond.java
@@ -3,6 +3,8 @@ package day0;
 public class JamesBond {
     public static void main(String[] args) {
         System.out.println("Java is cool language");
-        System.out.println("Phyton is not");
+        System.out.println("C++ is not");
+
+        System.out.println("Today is my Birthday.");
     }
 }
