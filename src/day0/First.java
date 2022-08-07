0c21632a1b94889884bb5a560a7036d709b21f9e
ayka2858 <105168256+ayka2858@users.noreply.github.com>
2022-08-07 14:50:29 +0300

    Create First.java

---
+++ src/day0/First.java
@@ -0,0 +1,8 @@
+package day0;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Today is Saturday");
+        System.out.println("Tomorrow is Sunday");
+    }
+}
